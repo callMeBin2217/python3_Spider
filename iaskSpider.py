dfafa81e9402cbea50768eefdfcafb1450d8de42
HuBin <343603146@qq.com>
2018-01-29 20:36:18 +0800

    2018.1.29

--- iaskSpider.py
+++ iaskSpider.py
@@ -1,44 +1,21 @@
 __author__='callMeBin'
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+#encoding:utf-8
 
 import urllib
 import urllib.request
 import re
-
+#解决中文乱码
+import sys
+import importlib 
+importlib.reload(sys) 
 
 '''
 目标：
 1.实现输入需要询问的问题的关键词，返回前10页所有询问，并保存到txt文档中
 2.保存的信息有：问题：问题的链接
 '''
-
-
-#工具类Tool，用于把内容中的各种标签去掉，保留最原始的文字
-class Tool():
-	#去除img标签，7空位长空格
-	removeImg = re.compile(r'<img.*?>| {7}|')
-	#去除超链接标签
-	removeAddr = re.compile(r'<a.*?>|</a>')
-	#把换行标签换位\n
-	replaceLine = re.compile(r'<tr>|<div>|</div>|</p>')
-	#将表格制表<td>换位\t
-	replaceTD = re.compile(r'<td>')
-	#把段落开头换位\n加两个空格
-	replacePara = re.compile(r'<p.*?>')
-	#将换行符号或多换行符换为\n
-	replaceBR = re.compile(r'<br>*')
-	#将其他标签剔除
-	removeExtraTag = re.compile(r'<.*?>')
-	def replace(self,x):
-		x = re.sub(self.removeImg,"",x)
-		x = re.sub(self.removeAddr,"",x)
-		x = re.sub(self.replaceLine,"\n",x)
-		x = re.sub(self.replaceTD,"\t",x)
-		x = re.sub(self.replacePara,"\n  ",x)
-		x = re.sub(self.replaceBR,'\n',x)
-		x = re.sub(self.removeExtraTag,"",x)
-		return x.strip()
 
 
 
@@ -52,8 +29,8 @@ class iaskSpider(object):
 		self.enable = False
 		self.base_url = r'https://iask.sina.com.cn'
 		self.keyWorld = keyWorld
-		self.searchWord = '/search?searchWord='+str(keyWorld)
-		self.tool = Tool()
+		#url中是不允许出现中文字符的，这时候就改用urllib.parse.quote方法对中文字符进行转换。
+		self.searchWord = '/search?searchWord='+urllib.parse.quote(str(keyWorld))
 		#初始化一个title
 		self.defaultTitle = 'unknown_Title'
 		#全局file，用于写入文件
@@ -67,6 +44,7 @@ class iaskSpider(object):
 		try:
 			#https://iask.sina.com.cn/search?searchWord=python&page=1
 			url = self.base_url+self.searchWord+'&page='+str(pageIndex)
+			print(url)
 			request = urllib.request.Request(url)
 			response = urllib.request.urlopen(request)
 			return response.read().decode('utf-8')
@@ -77,18 +55,24 @@ class iaskSpider(object):
 
 
 	def getContent(self,page):
-		patternContent = re.compile(r'<p class="title">[\s\S]*?<a href=(.*?)>[\s\S]*?(.*?)</a',re.S)
+		patternContent = re.compile(r'<p class="title">[\s\S]*?<a href="(.*?)".*?>[\s\S]*?(.*?)</a',re.S)
 		items = re.findall(patternContent,page)
 		#储存器
 		libaray = []
+		#取掉<span>
+		replaceSpan  = re.compile(r'<span.*?>|</span>')
 		for item in items:
-			#new_content = self.tool.replace(item)+'\n'
-			libaray.append(item)
+			#item返回的是一个tuple
+			new_item1 = re.sub(replaceSpan,"",item[1])
+			new_tuple = (item[0],new_item1)
+			libaray.append(new_tuple)
 		return libaray
 
 
 	def setFileTitle(self,title):
-		path = r'C:/Users/LENOVO/Desktop/'+title+'.txt'
+		'''文件要存放的地址'''
+		#path = r'C:/Users/LENOVO/Desktop/'+title+'.txt'
+		path=r'C:/Users/SleepAutumnD/Desktop/'+title+'.txt'
 		print(path)
 		self.file = open(path,'w+')
 
@@ -98,7 +82,8 @@ class iaskSpider(object):
 			foolLine = '\n'+str(self.floor)+u'=========================='+'\n'
 			try:
 				self.file.write(foolLine)
-				to_write = '问题:'+str(item[1]).strip()+'url地址:'+str(item[0]).strip()+'\n'
+				#构造字符串
+				to_write = '问题: '+str(item[1]).strip()+'\n'+'url地址: '+self.base_url+str(item[0]).strip()+'\n'
 				self.file.write(to_write)
 				self.floor +=1
 			except IOError as e:
